230de55d1a98a33f7534eaf2eb7b1faad8dc936c
Yiyi Wang <yiyi.wang@runtimeverification.com>
2018-08-30 19:16:07 +0000

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+
+sed -i 's/\.\.\\lib\\Remotery.h/\.\.\/lib\/Remotery.h/g' sample/sample.c 
+kcc -profile x86_64-linux-gcc-glibc-gnuc
+kcc -std=gnu11 lib/Remotery.c sample/sample.c -I ./lib -pthread -lm
+timeout 5s ./a.out
+
+rv-html-report $json_out -o $report_out
+rv-upload-report $report_out
