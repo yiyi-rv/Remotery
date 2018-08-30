25f45134f60b9bd88d3242810bd024f5f958c175
Yiyi Wang <yiyi.wang@runtimeverification.com>
2018-08-30 19:22:27 +0000

    Generate errors.json

--- rv.sh
+++ rv.sh
@@ -6,7 +6,7 @@ report_out=`pwd`/report
 
 sed -i 's/\.\.\\lib\\Remotery.h/\.\.\/lib\/Remotery.h/g' sample/sample.c 
 kcc -profile x86_64-linux-gcc-glibc-gnuc
-kcc -std=gnu11 lib/Remotery.c sample/sample.c -I ./lib -pthread -lm
+kcc -std=gnu11 lib/Remotery.c sample/sample.c -I ./lib -pthread -lm -fissue-report=$json_out
 timeout 5s ./a.out
 
 touch $json_out && rv-html-report $json_out -o $report_out
